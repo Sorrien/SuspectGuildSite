3298ba500b184494f54b523cb145f5c5a855cc02
Sorrien <collin.sparks@gmail.com>
2022-03-01 17:57:43 -0500

    initial layout

---
+++ SuspectWebApp.Api/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(SuspectWebApp.Api.Startup))]
+
+namespace SuspectWebApp.Api
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            
+        }
+    }
+}
--- SuspectWebApp.Shared/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SuspectWebApp.Shared
-{
-    public class Class1
-    {
-
-    }
-}
